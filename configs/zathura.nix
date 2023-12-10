6d82add0a4673e4612b2be0b8eba779a5b43aa9b
Daniel Gilleran <dangilleran7@protonmail.com>
2023-12-09 22:46:18 -0500

    configured zathura

--- configs/zathura.nix
+++ configs/zathura.nix
@@ -6,27 +6,34 @@ let
 in {
   programs.zathura = {
     enable = true;
+
+
+    mappings = {
+      "<Right>" = "navigate next";
+      "<Left>" = "navigate previous";
+    };
+    
     options = {
       default-bg = colors.kanagawa.bg;
       default-fg = colors.kanagawa.fg;
       statusbar-fg = colors.kanagawa.fg;
       statusbar-bg = colors.kanagawa.bg;
-      inputbar-bg = colors.kanagawa.red;
-      inputbar-fg = colors.kanagawa.red;
+      inputbar-bg = colors.kanagawa.bg;
+      inputbar-fg = colors.kanagawa.fg;
       notification-bg = colors.kanagawa.red;
       notification-fg = colors.kanagawa.red;
       notification-error-bg = colors.kanagawa.red;
       notification-error-fg = colors.kanagawa.red;
       notification-warning-bg = colors.kanagawa.red;
       notification-warning-fg = colors.kanagawa.red;
-      highlight-color = colors.kanagawa.red;
-      highlight-active-color = colors.kanagawa.red;
-      completion-bg = colors.kanagawa.red;  
-      completion-fg = colors.kanagawa.red;
-      completion-highlight-fg = colors.kanagawa.red;
-      completion-highlight-bg =  colors.kanagawa.red;
-      recolor-lightcolor = colors.kanagawa.red;
-      recolor-darkcolor = colors.kanagawa.red;
+      highlight-color = colors.kanagawa.blue;
+      highlight-active-color = colors.kanagawa.white;
+      completion-bg = colors.kanagawa.bg;  
+      completion-fg = colors.kanagawa.fg;
+      completion-highlight-fg = colors.kanagawa.white;
+      completion-highlight-bg =  colors.kanagawa.bg;
+      recolor-lightcolor = colors.kanagawa.bg;
+      recolor-darkcolor = colors.kanagawa.white;
     };
   };
 }
