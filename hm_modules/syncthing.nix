fe316416ee8b76efd0fb1cb5c7caebaffee5122f
Daniel Gilleran <dangilleran7@protonmail.com>
2025-03-22 00:59:02 -0400

    removed declarative syncthing folder setup (was a pain)

--- hm_modules/syncthing.nix
+++ hm_modules/syncthing.nix
@@ -3,6 +3,7 @@
 {
   services.syncthing = {
     enable = true;
+    tray.enable = true;
     guiAddress = "127.0.0.1:8384";
     settings = {
       devices = {
@@ -14,58 +15,58 @@
           id = "R2L6NI3-MWEJZQY-TWEX2NC-IA2IY27-4IJVUGP-M6YOERD-5YFJUAN-KMCLMAT";
           };
       };
-      folders = {
-        "/home/daniel/Documents" = {
-          id = "Documents";
+      # folders = {
+      #   "/home/daniel/Documents" = {
+      #     id = "Documents";
           
-          devices = ["chaucer" "melville"]; # without this, the folder will exist on all devices but not be shared
-          # trashed files stored in .stverions
-          versioning = {
-            type = "trashcan";
-            params.cleanoutDays = "1000";
-            };
-          };
-        };
-        "/home/daniel/Pictures" = {
-          id = "Pictures";
+      #     devices = ["chaucer" "melville"]; # without this, the folder will exist on all devices but not be shared
+      #     # trashed files stored in .stverions
+      #     versioning = {
+      #       type = "trashcan";
+      #       params.cleanoutDays = "1000";
+      #       };
+      #     };
+      #   };
+      #   "/home/daniel/Pictures" = {
+      #     id = "Pictures";
           
-          devices = ["chaucer" "melville"]; # without this, the folder will exist on all devices but not be shared
-          # trashed files stored in .stverions
-          versioning = {
-            type = "trashcan";
-            params.cleanoutDays = "1000";
-            };
-          };
-        "/home/daniel/Videos" = {
-          id = "Videos";
+      #     devices = ["chaucer" "melville"]; # without this, the folder will exist on all devices but not be shared
+      #     # trashed files stored in .stverions
+      #     versioning = {
+      #       type = "trashcan";
+      #       params.cleanoutDays = "1000";
+      #       };
+      #     };
+      #   "/home/daniel/Videos" = {
+      #     id = "Videos";
           
-          devices = ["chaucer" "melville"]; # without this, the folder will exist on all devices but not be shared
-          # trashed files stored in .stverions
-          versioning = {
-            type = "trashcan";
-            params.cleanoutDays = "1000";
-            };
-          };
-        "/home/daniel/src" = {
-          id = "src";
+      #     devices = ["chaucer" "melville"]; # without this, the folder will exist on all devices but not be shared
+      #     # trashed files stored in .stverions
+      #     versioning = {
+      #       type = "trashcan";
+      #       params.cleanoutDays = "1000";
+      #       };
+      #     };
+      #   "/home/daniel/src" = {
+      #     id = "src";
           
-          devices = ["chaucer" "melville"]; # without this, the folder will exist on all devices but not be shared
-          # trashed files stored in .stverions
-          versioning = {
-            type = "trashcan";
-            params.cleanoutDays = "1000";
-            };
-          };
-        "/home/daniel/Calibre_Library" = {
-          id = "Calibre Library";
+      #     devices = ["chaucer" "melville"]; # without this, the folder will exist on all devices but not be shared
+      #     # trashed files stored in .stverions
+      #     versioning = {
+      #       type = "trashcan";
+      #       params.cleanoutDays = "1000";
+      #       };
+      #     };
+      #   "/home/daniel/Calibre_Library" = {
+      #     id = "Calibre Library";
           
-          devices = ["chaucer" "melville"]; # without this, the folder will exist on all devices but not be shared
-          # trashed files stored in .stverions
-          versioning = {
-            type = "trashcan";
-            params.cleanoutDays = "1000";
-            };
-          };
+      #     devices = ["chaucer" "melville"]; # without this, the folder will exist on all devices but not be shared
+      #     # trashed files stored in .stverions
+      #     versioning = {
+      #       type = "trashcan";
+      #       params.cleanoutDays = "1000";
+      #       };
+      #     };
         };
       };
 }
